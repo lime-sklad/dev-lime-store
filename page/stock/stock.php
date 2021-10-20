e556411b5073ce59716a9420176fa20cfc4ab410
lime-sklad <emil.gasanovua@gmail.com>
2021-10-20 11:17:49 +0400

    product update modal form

--- page/stock/stock.php
+++ page/stock/stock.php
@@ -57,10 +57,7 @@
 				'table_type' => $type,
 			],
 			'/component/table/table_footer_wrapper.twig' => [
-				'table_total' => get_table_total([
-									'total_list' => $page_config['table_total_list'],  
-									'data' => $table_data['base_result']
-								])
+				'table_total' => table_footer_result($page_config['table_total_list'], $table_data['base_result'])
 			]
 		]
 	]);
